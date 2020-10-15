8e3376c0155dc0fd88742194d217004da3df734a
karl-thomas <karl.thomas925@gmail.com>
2020-10-14 21:58:47 -0500

    create compound component to share implicit state between children

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -75,7 +75,7 @@ function useUser() {
 
 function updateUser(dispatch, user, updates) {
   dispatch({type: 'start update', updates})
-  userClient.updateUser(user, updates).then(
+  return userClient.updateUser(user, updates).then(
     updatedUser => dispatch({type: 'finish update', updatedUser}),
     error => dispatch({type: 'fail update', error}),
   )
