fbef7963f0d93a788638716c59f53d90a544f406
karl-thomas <karl.thomas925@gmail.com>
2020-10-14 22:04:03 -0500

    account for if a child is a DOM element

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -8,32 +8,24 @@ function Toggle({children}) {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
 
-  // 🐨 replace this with a call to React.Children.map and map each child in
-  // props.children to a clone of that child with the props they need using
-  // React.cloneElement.
-  // 💰 React.Children.map(props.children, child => {/* return child clone here */})
-  // 📜 https://reactjs.org/docs/react-api.html#reactchildren
-  // 📜 https://reactjs.org/docs/react-api.html#cloneelement
   return React.Children.map(children, child =>
-    React.cloneElement(child, {on, toggle}),
+    typeof child.type === 'string'
+      ? child
+      : React.cloneElement(child, {on, toggle}),
   )
 }
 
-// 🐨 Flesh out each of these components
-
-// Accepts `on` and `children` props and returns `children` if `on` is true
 const ToggleOn = ({on, children}) => (on ? children : null)
 
-// Accepts `on` and `children` props and returns `children` if `on` is false
 const ToggleOff = ({on, children}) => (on ? null : children)
 
-// Accepts `on` and `toggle` props and returns the <Switch /> with those props.
 const ToggleButton = ({on, toggle}) => <Switch on={on} onClick={toggle} />
 
 function App() {
   return (
     <div>
       <Toggle>
+        <p>don't mind me</p>
         <ToggleOn>The button is on</ToggleOn>
         <ToggleOff>The button is off</ToggleOff>
         <ToggleButton />
